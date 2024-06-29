2abc7bfc2f58d912b2233bf08fe2c994f906af96
WangZhaoXiong <wangzhaoxiong026@163.com>
2024-06-29 11:50:19 +0800

    thrid commit

--- src/main/java/com/wang/Test1.java
+++ src/main/java/com/wang/Test1.java
@@ -4,5 +4,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("hellow git");
         System.out.println("hellow git2");
+        System.out.println("hellow git3");
     }
 }
