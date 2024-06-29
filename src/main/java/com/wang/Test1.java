3a90369cf3e3b7a73be5c1b5e369eead87025479
WangZhaoXiong <wangzhaoxiong026@163.com>
2024-06-29 15:15:26 +0800

    push commit2

--- src/main/java/com/wang/Test1.java
+++ src/main/java/com/wang/Test1.java
@@ -9,5 +9,6 @@ public class Test1 {
         System.out.println("hellow git5");
         System.out.println("hellow master");
         System.out.println("push test");
+        System.out.println("push test2");
     }
 }
